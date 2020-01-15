27a7ea72f5d15b365a4b05d03ad2fe3c07225943
hamada <hamashun18@yahoo.co.jp>
2020-01-15 13:37:40 +0900

    ログイン機能の追加、ログイン中のユーザー名の表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
   end
+
+  def create
+    Posts.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content).merge(user_id: current_user.id)
+  end
+
+
 end
